a1ae1e7184d19d985a88617c2dc623873c59955d
Deepesh Kumar <thakurdeepesh045@gmail.com>
2026-03-13 14:51:05 +0530

    Fix upload path variable

--- src/main/java/com/free/productSolution/config/WebConfig.java
+++ src/main/java/com/free/productSolution/config/WebConfig.java
@@ -35,11 +35,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/fastners/**")
                 .addResourceLocations("file:" + absolutePath + "/fastners/");
         
-     // Serve all /uploads/** URLs from this folder
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadDir + "/");
-
-        
     }
 
 }
