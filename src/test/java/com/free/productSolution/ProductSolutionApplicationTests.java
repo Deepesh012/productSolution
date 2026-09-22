48f26e437334ceaf18de86d99ea066c216163917
Deepesh Kumar <Deepesh012-thakurdeepesh045@gmail.com>
2026-09-22 16:09:21 +0530

    Configure test profile and maven test environment

--- src/test/java/com/free/productSolution/ProductSolutionApplicationTests.java
+++ src/test/java/com/free/productSolution/ProductSolutionApplicationTests.java
@@ -2,8 +2,10 @@ package com.free.productSolution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductSolutionApplicationTests {
 
 	@Test
